162433b01bbeea7e739a8e245a9cf0f60492429c
Steven Rummler <steven.rummler@gmail.com>
2023-02-25 20:45:15 -0700

    don't insert on each read

--- pages/api/loci.ts
+++ pages/api/loci.ts
@@ -4,7 +4,6 @@ import { lociConnection } from "./mongo"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const loci = await lociConnection;
-  await loci.insertOne({ timestamp: Date.now()});
   const results = await loci.find({}).toArray();
   res.status(200).json(results);
 }
